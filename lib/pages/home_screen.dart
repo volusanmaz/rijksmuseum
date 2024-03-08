e809d00d6f92cfaad39fed4070c4e8fede2ee665
volusanmaz <102143944+volusanmaz@users.noreply.github.com>
2024-03-08 18:06:13 +0300

    Cube Transform effect done with a 3rd party library...

---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Text(
+          'Home Screen',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cube_transition_plus/cube_transition_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:rijksmuseum/main.dart';
+import 'package:rijksmuseum/pages/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,8 +11,39 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  void initState() {
+    super.initState();
+
+    // Wait for 3 seconds and then navigate to the home screen
+    if(!MyApp.HomeScreenShown) {
+      Future.delayed(Duration(seconds: 3), () {
+        Navigator.of(context).push(
+          CubePageRoute(
+            enterPage: HomeScreen(),
+            exitPage: SplashScreen(),
+            duration: const Duration(milliseconds: 900),
+          ),
+        );
+      });
+      MyApp.HomeScreenShown=true;
+
+
+
+    }}
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Text(
+          'Splash Screen',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        ),
+      ),
+    );
   }
 }
